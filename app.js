4e11fe8ce8bdab568cd25043740754a504dc9c2f
Kaare Blakstad <kare.blakstad@finn.no>
2013-08-15 12:11:17 +0200

    added mmm for mustache templating, and bootstrap sources

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
-var express = require('express');
-var http = require('http');
+var express = require('express')
+  , routes  = require('./routes')
+  , index   = require('./routes/index')
+  , http    = require('http')
+  , path    = require('path');
 
 var app = express();
-
-app.engine('html', require('hogan-express'));
-app.enable('view cache');
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'html');
+  app.set('view engine', 'mmm');
+  app.set('layout', 'layout');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('lalashere'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
+  app.enable('view cache');
 });
 
-app.get('/', function(req, res) {
-	res.render('index', {msg: 'finnsect'})
-});
+app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
