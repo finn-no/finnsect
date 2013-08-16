19b73f0cab74dbb955abb6f7ce51c4f5450f3f14
Kaare Blakstad <kare.blakstad@finn.no>
2013-08-16 17:07:50 +0200

    http client optimalization to prevent exhaustion of reponse body pool

--- httpr.js
+++ httpr.js
@@ -1,14 +1,16 @@
 var http = require('http');
 
+http.globalAgent.maxSockets = 100;
+
 exports.resources = function(rs, callback) {
 	var len = rs.length;
 	rs.forEach(function(r) {
-		var req = http.request(
-			{
+		var req = http.request({
 			  host: r.host,
 			  port: 80,
 			  path: r.path,
-			  method: r.method
+			  method: r.method,
+			  agent: false
 			}, 
 			function(response) {
 				r.actualStatusCode = response.statusCode;
@@ -22,6 +24,7 @@ exports.resources = function(rs, callback) {
 		req.on('error', function(e) {
 			console.log('[http] problem accessing ' + r.host + r.path);
 		});
+		req.on('data', function() { /* /dev/null */ });
 		req.end();
 	});
 };
